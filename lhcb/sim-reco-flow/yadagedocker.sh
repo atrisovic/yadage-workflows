e60f4d8f77d641e517a26ae8e9ce656f7a554fe0
Ana Trisovic <atrisovic@users.noreply.github.com>
2016-11-29 15:24:07 +0100

    yadagedocker.sh

---
+++ lhcb/sim-reco-flow/yadagedocker.sh
@@ -0,0 +1,3 @@
+function yadage-run {
+   docker run --rm -it -e PACKTIVITY_DOCKER_NOPULL=true -e PACKTIVITY_WITHIN_DOCKER=true -v $PWD:$PWD -w $PWD -v /var/run/docker.sock:/var/run/docker.sock lukasheinrich/yadage yadage-run $*
+}
